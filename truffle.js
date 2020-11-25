91d02a8901b880baecb10b093d76c9cdc37b45d1
Stephen Vanderwarker <1-stephen@users.noreply.gitlab.vanderwarker.dev>
2020-11-24 21:41:59 -0500

    Updating package.json to include truffle-hdwallet, and dotenv specifying solc version in truffle.js, adding ropsten, using Infura API

--- truffle.js
+++ truffle.js
@@ -1,19 +1,30 @@
-// Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+require('dotenv').config() ;
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.MNENOMIC;
 
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-      gas: 4704624
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
     },
     ropsten: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '3',
-      gas: 4700000
+     provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+     network_id: 3,       // Ropsten's id
+     gas: 5500000,        // Ropsten has a lower block limit than mainnet
     }
+  },
+compilers: {
+    solc: {
+      version: "0.4.13",
+    },
   }
-}
+};
